b89ee7521a648f303e0fad9c726e7f7c488159df
Roman Studenov <studenovroman@yandex.ru>
2020-06-12 22:32:16 +0700

    feature добавил компонент Content

---
+++ src/components/Content/index.tsx
@@ -0,0 +1,54 @@
+import React, { FC, memo } from "react";
+import Grid from "hedron";
+
+import { TextStyled } from "./styles";
+
+const Content: FC = () => (
+  <Grid.Box width="calc(100% - 150px)">
+    <TextStyled>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus at odio
+      reprehenderit, a accusamus, ab nostrum expedita nam recusandae voluptates
+      quidem blanditiis dolor, in tempore magnam dicta exercitationem
+      voluptatem? Fuga?
+    </TextStyled>
+    <TextStyled>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat quod
+      laudantium aliquid, iusto, iure quidem deserunt ratione neque distinctio
+      assumenda nostrum. Cupiditate fugiat, ipsa labore architecto ad animi
+      similique? Cum vel odio libero, autem modi quod obcaecati maiores quis
+      architecto quam, cumque ad nihil necessitatibus aliquam laboriosam. Magni
+      ipsam mollitia porro, et aliquam accusamus temporibus. Ex optio deleniti
+      ullam laudantium! Quam non, quod, culpa unde assumenda optio, asperiores
+      ratione esse sequi ex hic adipisci aliquid similique deleniti cumque
+      dignissimos voluptatem eum fugit repellendus maxime laudantium vero dicta?
+      Totam ratione repudiandae aspernatur reprehenderit explicabo officiis
+      ipsam aliquam? Aliquid harum deleniti quae.
+    </TextStyled>
+    <TextStyled>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores
+      ratione aut minus modi vitae cupiditate adipisci est non dolor accusantium
+      ipsam iure odio amet animi quidem molestiae quis, excepturi expedita
+      libero repudiandae. Ab mollitia architecto quam itaque, ratione sed ad
+      quaerat cupiditate impedit harum quo velit officiis rerum possimus totam
+      in, non cum nisi aliquid illum quia modi. Veniam, nemo? Et delectus
+      reiciendis inventore minima assumenda perspiciatis. Laborum natus fugit
+      eaque consectetur nesciunt, minus magni nihil eligendi commodi placeat est
+      error officia libero debitis cupiditate ipsam corrupti itaque, odio
+      fugiat, praesentium minima pariatur laudantium! Neque explicabo iste alias
+      reprehenderit quo nisi minima asperiores. At perspiciatis dolorum quis
+      voluptates incidunt, quod praesentium sunt libero odio culpa ullam
+      blanditiis placeat neque, minima excepturi dignissimos? Tenetur,
+      voluptatibus autem amet reiciendis qui nisi quis. Rem necessitatibus id
+      tempora molestiae, omnis harum sapiente nihil facilis laboriosam deserunt
+      quas laudantium pariatur. Ullam laboriosam quod eaque tempore? Itaque
+      sequi alias, amet unde tenetur eaque aliquid dolores laboriosam, maiores
+      nam magni. Recusandae velit cupiditate fuga esse alias molestiae
+      exercitationem magni? Perferendis, magnam odit. Expedita et laudantium
+      reprehenderit vitae suscipit assumenda, optio voluptas possimus facilis
+      minima culpa, dolorum accusantium natus facere qui dicta nobis est officia
+      sed placeat nihil?
+    </TextStyled>
+  </Grid.Box>
+);
+
+export default memo(Content);
---
+++ src/components/Content/styles.ts
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const TextStyled = styled.p`
+  font-size: 14px;
+  line-height: 16px;
+  margin: 5px;
+  padding: 0 5px;
+`;
